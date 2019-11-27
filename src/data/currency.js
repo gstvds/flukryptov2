c58bc4e022aa62941008293d9ce0c2e788a41255
Gustavo da Silva <gstvds@icloud.com>
2019-11-27 18:44:10 -0300

    Add 20 most used coins + brl
    This is used to fecth the data and render inside the picker

---
+++ src/data/currency.js
@@ -0,0 +1,31 @@
+export const Currency = [
+  { label: "Real", value: "BRL" },
+  { label: "Euro", value: "EUR" },
+  { label: "Dólar Americano", value: "USD" },
+  { label: "Libra Esterlina", value: "GBP" },
+  { label: "Iene", value: "JPY" },
+  { label: "Dólar Australiano", value: "AUD" },
+  { label: "Franco Suiço", value: "CHF" },
+  { label: "Dólar Canadense", value: "CAD" },
+  { label: "Dólar de Cingapura", value: "SGD" },
+  { label: "Dólar de Hong Kong", value: "HKD" },
+  { label: "Baht", value: "THB" },
+  { label: "Coroa Sueca", value: "SEK" },
+  { label: "Coroa Norueguesa", value: "NOK" },
+  { label: "Peso Argentino", value: "ARS" },
+  { label: "Yuan", value: "CNY" },
+  { label: "Coroa Dinamarquesa", value: "DKK" },
+  { label: "Rublo Russo", value: "RUB" },
+  { label: "Rand", value: "ZAR" },
+  { label: "Dólar Neozelandês", value: "NZD" },
+  { label: "Peso Mexicano", value: "MXN" },
+  { label: "Lira Turca", value: "TRY" },
+  { label: "Forint", value: "HUF" }
+];
+
+export const Data = [
+  { label: "Ontem", value: "1" },
+  { label: "Últimos 7 dias", value: "7" },
+  { label: "Últimos 15 dias", value: "15" },
+  { label: "Últimos 30 dias", value: "30" }
+];
