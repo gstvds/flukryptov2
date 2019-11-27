3a82515c2a7711a7463cc864fdd4e92a667c1a3d
Gustavo da Silva <gstvds@icloud.com>
2019-11-27 18:45:05 -0300

    Add models to search data

---
+++ src/models/initial-data.js
@@ -0,0 +1,9 @@
+class Initial {
+  constructor(data, usd, eur) {
+    this.data = data;
+    this.usd = usd;
+    this.eur = eur;
+  }
+}
+
+export default Initial;
---
+++ src/models/search-data.js
@@ -0,0 +1,12 @@
+class Search {
+  constructor(timeFrom, timeTo, high, low, open, close) {
+    this.timeFrom = timeFrom;
+    this.timeTo = timeTo;
+    this.high = high;
+    this.low = low;
+    this.open = open;
+    this.close = close;
+  }
+}
+
+export default Search;
